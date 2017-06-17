8874193e7692e26ebcd756bdbf21d59b72c07610
rayGreycloud <rayGreycloud@gmail.com>
2017-06-17 08:08:13 -0700

    Add function to show domain and range of each data object

--- js/main2.js
+++ js/main2.js
@@ -6,7 +6,12 @@ d3.json("data/age.json", function(d) {
     .domain([500000, 6000000])
     .range([50, 500])
 
-  console.log(scale(500000));
-  console.log(scale(6000000));
-  console.log(scale(3000000));
+  // console.log(scale(500000));
+  // console.log(scale(6000000));
+  // console.log(scale(3000000));
+
+  d.forEach(function (a) {
+    console.log(`domain: ${a.total}`);
+    console.log(`range: ${scale(a.total)}`);
+  });
 });
