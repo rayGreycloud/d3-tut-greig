6a29b3bce1bf91ad4f04c9e6d55a17da3d74b6ef
rayGreycloud <rayGreycloud@gmail.com>
2017-06-21 14:25:57 -0700

    Load data, create svg and render

---
+++ pop-by-state/js/main.js
@@ -0,0 +1,12 @@
+// Initiate, render and handle input
+
+d3.queue()
+  .defer(d3.json, "data/state_pop.json")
+  .await(function (error, d) {
+    // Create svg element
+    barGraph.container.svg = d3.select("#graph").append("svg")
+      .attr("height", barGraph.container.height)
+      .attr("width", barGraph.container.width);
+
+    barGraph.render(d, "total");
+  });
